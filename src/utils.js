711326223e8ef25e217a5afc68ccf8c749da04de
ceccagabry <gabriella.ceccarelli.83@gmail.com>
2023-11-18 11:45:28 +0100

    first valid date shall be 29 days past current date. It used to be 28

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 // utils.js
 function getNextValidDates(inputDate, numberOfDates = 3) {
     const daysToCheck = [2, 3, 4]; // Tuesday, Wednesday, Thursday represented as 2, 3, 4 (respectively)
-    const addDays = [28, 1, 1]; // Days to add for each date
+    const addDays = [29, 1, 1]; // Days to add for each date
   
     const getNextValidDate = (startDate, daysToAdd) => {
       let nextDate = new Date(startDate.getTime() + daysToAdd * 24 * 60 * 60 * 1000);
