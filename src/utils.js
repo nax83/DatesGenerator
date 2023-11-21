5e85b002d26f91174c136117a7de7a3da1976fa0
Nax83 <ale.nassisi@gmail.com>
2023-11-21 10:28:22 +0100

    refactor getNextValidDates to support adding custom minimum amount of days

--- src/utils.js
+++ src/utils.js
@@ -1,26 +1,31 @@
 // utils.js
-function getNextValidDates(inputDate, numberOfDates = 3) {
-    const daysToCheck = [2, 3, 4]; // Tuesday, Wednesday, Thursday represented as 2, 3, 4 (respectively)
-    const addDays = [29, 1, 1]; // Days to add for each date
-  
-    const getNextValidDate = (startDate, daysToAdd) => {
-      let nextDate = new Date(startDate.getTime() + daysToAdd * 24 * 60 * 60 * 1000);
-      while (!daysToCheck.includes(nextDate.getUTCDay())) {
-        nextDate = new Date(nextDate.getTime() + 24 * 60 * 60 * 1000);
-      }
-      return nextDate;
-    };
-  
-    const validDates = [];
-    let currentDate = inputDate;
-  
-    for (let i = 0; i < numberOfDates; i++) {
-      currentDate = getNextValidDate(currentDate, addDays[i]);
-      validDates.push(currentDate);
+function weeksToDays(numberOfWeeks) {
+  return numberOfWeeks * 7 + 1;
+}
+
+function getNextValidDates(inputDate, numberOfDates = 3, minWeeks = 4) {
+  const daysToCheck = [2, 3, 4]; // Tuesday, Wednesday, Thursday represented as 2, 3, 4 (respectively)
+  const minDays = weeksToDays(minWeeks); // Convert minWeeks to minDays
+  const addDays = [minDays, 1, 1]; //Minimum number of days to add for each date
+
+  const getNextValidDate = (startDate, daysToAdd) => {
+    let nextDate = new Date(startDate.getTime() + daysToAdd * 24 * 60 * 60 * 1000);
+    while (!daysToCheck.includes(nextDate.getUTCDay())) {
+      nextDate = new Date(nextDate.getTime() + 24 * 60 * 60 * 1000);
     }
-  
-    return validDates;
+    return nextDate;
+  };
+
+  const validDates = [];
+  let currentDate = inputDate;
+
+  for (let i = 0; i < numberOfDates; i++) {
+    currentDate = getNextValidDate(currentDate, addDays[i]);
+    validDates.push(currentDate);
   }
+
+  return validDates;
+}
   
-  export { getNextValidDates };
+export { getNextValidDates, weeksToDays };
   
