6af9d7d01474cd144b7f42d0b00cb2f957e59421
ceccagabry <gabriella.ceccarelli.83@gmail.com>
2023-11-23 07:02:29 +0100

    first therapy planning implementation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-// App.js
 import React from 'react';
 import './App.css';
-import DatesList from './DatesList';
+import EyeTherapy from './EyeTherapy'; // Assuming EyeTherapy component is available
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import DatesList from './DatesList';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World! This is a React App</h1>
-      <DatesList />
+      <Container>
+        <Row>
+          <Col>
+              <EyeTherapy title="Left Eye Therapy" maxInjections={5} />
+          </Col>
+          <Col>
+              <EyeTherapy title="Right Eye Therapy" maxInjections={5} />
+          </Col>
+        </Row>
+      </Container>
+      <DatesList/>
     </div>
   );
 }
---
+++ src/EyeTherapy.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import Dropdown from 'react-bootstrap/Dropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function EyeTherapy({ title, maxInjections }) {
+    const [selectedInjections, setSelectedInjections] = useState(0);
+    const [injections, setInjections] = useState(Array.from({ length: maxInjections }, (_, i) => ({ id: i, weeks: 0 })));
+  
+    const handleDropdownSelect = (eventKey, event) => {
+      const selectedId = parseInt(event.target.getAttribute('data-id'));
+  
+      if (selectedId !== undefined) {
+        const updatedInjections = injections.map((injection) =>
+          injection.id === selectedId ? { ...injection, weeks: parseInt(eventKey) } : injection
+        );
+  
+        setInjections(updatedInjections);
+      }
+  
+      setSelectedInjections(eventKey);
+    };
+
+    const injectionsDropdown = injections
+    .filter((injection) => injection.id < selectedInjections)
+    .map((injection) => (
+      <Dropdown key={injection.id}>
+        <Dropdown.Toggle variant="success" id={`dropdown-injection-${injection.id}`}>
+          Injection {injection.id + 1}: Wait for {injection.weeks} weeks
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          {[...Array(maxInjections + 1).keys()].slice(1).map((week) => (
+            <Dropdown.Item
+              key={week}
+              eventKey={week}
+              data-id={injection.id}
+              onSelect={handleDropdownSelect}
+            >
+              {week} weeks
+            </Dropdown.Item>
+          ))}
+        </Dropdown.Menu>
+      </Dropdown>
+    ));
+
+  return (
+    <Card>
+      <Card.Header>{title}</Card.Header>
+      <Card.Body>
+        <Dropdown onSelect={handleDropdownSelect}>
+          <Dropdown.Toggle variant="primary" id="dropdown-basic">
+            Number of Injections: {selectedInjections}
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            {[...Array(maxInjections + 1).keys()].map((injection) => (
+              <Dropdown.Item key={injection} eventKey={injection}>
+                {injection}
+              </Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
+        {injectionsDropdown}
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default EyeTherapy;
+
